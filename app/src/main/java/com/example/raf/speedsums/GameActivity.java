8aa3b6bbfb73b00b7e542d854d95102988c1010f
Raf <rkrassowski@gmail.com>
2017-01-31 14:38:54 +0000

    initial commit

--- app/src/main/java/com/example/raf/speedsums/GameActivity.java
+++ app/src/main/java/com/example/raf/speedsums/GameActivity.java
@@ -12,9 +12,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Collections;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -23,91 +20,85 @@ public class GameActivity extends AppCompatActivity {
     TextView rightWrongText;
     TextView sumTextView;
     TextView scoreTextView;
-    Button button4;
+    Button button0;
     Button button1;
     Button button2;
     Button button3;
+    Button tryAgainButton;
     int currentScore;
     int numberOfAttempts;
-    int maxGeneratedNumber;
     int generatedRandom;
-    int minGeneratedNumber = 1;
     int randomSum;
     List<Integer> randomsList;
     List<Integer> randomSumList;
+    int score = 0;
+    int r;
+    int randomSumPosition;
+    int totalQuestions = 0;
 
     public void randomGenerate() {
 
-        maxGeneratedNumber = 30;
-
         randomsList = new ArrayList<Integer>();
+        Random r = new Random();
+
+        int a = r.nextInt(12);
+        int b = r.nextInt(12);
+        randomSum = a+b;
+        randomsList.add(randomSum);
+        sumTextView.setText(a + "+" + b);
 
         for (int i = 0; i <= 2; i++) {
-            Random r = new Random(maxGeneratedNumber);
-            randomsList.add(generatedRandom);
-        }
-    }
-
-        public int randomSumGenerate(){
-
-            maxGeneratedNumber = 12;
-
-            randomSumList = new ArrayList<Integer>();
-
-            for (int i = 0; i <= 1; i++) {
-                Random r = new Random();
-                randomSumList.add(generatedRandom);
+            generatedRandom = r.nextInt(30);
+            if(generatedRandom != randomSum){
+                randomsList.add(generatedRandom);
             }
-
-        sumTextView.setText(randomSumList.get(1) + "+" + randomSumList.get(0));
-        randomSum = randomSumList.get(1) + randomSumList.get(0);
-        return(randomSum);
-    }
-
-    public void assignValueToButton(){
-
-        randomsList.add(3, randomSum);
-        Collections.shuffle(randomsList);
-
-        button1.setText(randomsList.get(0).toString());
-        button2.setText(randomsList.get(1).toString());
-        button3.setText(randomsList.get(2).toString());
-        button4.setText(randomsList.get(3).toString());
-
-    }
-
-    public void countScore(){
-
-        //compare scores
-
-        scoreTextView.setText(currentScore + "/" + numberOfAttempts);
-
-    }
-
-
-    public void selectAnswer(View view) throws InterruptedException {
-
-
-        if ((String)view.getTag() == "1" && button1.getText() == Integer.toString(randomSum)){
-            rightWrongText.setText("Correct!");
-        } else if((String)view.getTag() == "2" && button2.getText() == Integer.toString(randomSum)){
-            rightWrongText.setText("Correct!");
-        } else if ((String)view.getTag() == "3" && button3.getText() == Integer.toString(randomSum)){
-            rightWrongText.setText("Correct!");
-        } else if((String)view.getTag() == "4" && button4.getText() == Integer.toString(randomSum)){
-            rightWrongText.setText("Correct!");
-        } else{
-            rightWrongText.setText("Wrong!");
         }
+    }
+
+    public void assignValueToButton() {
+
+        button0.setText(randomsList.get(0).toString());
+        button1.setText(randomsList.get(1).toString());
+        button2.setText(randomsList.get(2).toString());
+        button3.setText(randomsList.get(3).toString());
 
 
-                Log.i("Button pressed", (String)view.getTag());
-                randomGenerate();
-                randomSumGenerate();
-                assignValueToButton();
-                Log.i("random sum" , Integer.toString(randomSum));
+        randomSumPosition = randomsList.indexOf(randomSum);
+
+        Log.i("arrayList" , randomsList.toString());
+
+    }
 
 
+    public void selectAnswer(View view) {
+
+
+
+        Log.i("rs index", Integer.toString(randomSumPosition));
+        Log.i("Button pressed", (String)view.getTag());
+        Log.i("rs value", Integer.toString(randomSum));
+
+        if (view.getTag().toString().equals(Integer.toString(randomSumPosition))){
+            rightWrongText.setText("Correct!");
+            score++;
+            Log.i("a", "correct");
+        } else {
+            rightWrongText.setText("Wrong!");
+            Log.i("a", "wrong");
+        }
+        totalQuestions++;
+        scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(totalQuestions));
+
+        randomGenerate();
+        assignValueToButton();
+
+
+    }
+
+    public void tryAgain (View view){
+
+        Intent myIntent = new Intent(GameActivity.this, MainActivity.class);
+        GameActivity.this.startActivity(myIntent);
 
     }
 
@@ -117,22 +108,30 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        button0 = (Button)findViewById(R.id.button0);
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
         button3 = (Button)findViewById(R.id.button3);
-        button4 = (Button)findViewById(R.id.button4);
+
+        tryAgainButton = (Button)findViewById(R.id.tryAgainButton);
 
         timerTextView = (TextView)findViewById(R.id.timerTextView);
         rightWrongText = (TextView)findViewById(R.id.rightWrongText);
         sumTextView = (TextView)findViewById(R.id.sumTextView);
         scoreTextView = (TextView)findViewById(R.id.scoreTextView);
-        randomGenerate();
-        randomSumGenerate();
-        assignValueToButton();
-        scoreTextView.setText("0" + "/" + "0");
+
+        scoreTextView.setText("0" + "/" + "1");
         rightWrongText.setText("Good Luck!");
+        tryAgainButton.setVisibility(View.INVISIBLE);
+
+        button0.setClickable(true);
+        button1.setClickable(true);
+        button2.setClickable(true);
+        button3.setClickable(true);
 
 
+        randomGenerate();
+        assignValueToButton();
 
         countDownTimer = new CountDownTimer(30000, 1000) {
             @Override
@@ -149,15 +148,16 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
 
-                rightWrongText.setText("Game Over!");
+                timerTextView.setText("0");
 
-                new Timer().schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        Intent myIntent = new Intent(GameActivity.this, MainActivity.class);
-                        GameActivity.this.startActivity(myIntent);
-                    }
-                }, 5000);
+                rightWrongText.setText(scoreTextView.getText());
+
+                tryAgainButton.setVisibility(View.VISIBLE);
+
+                button0.setClickable(false);
+                button1.setClickable(false);
+                button2.setClickable(false);
+                button3.setClickable(false);
 
             }
         }.start();
